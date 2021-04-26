de285a80618b1a8be9e5ecfb41525a493ec7c51c
Maxime Guichard <maxime.guichard45@gmail.com>
2021-04-26 11:57:22 +0200

    quete 5.1 composer

---
+++ composer-quests-master/public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Wcs\Hello;
+use HelloWorld\SayHello;
+
+// $hello1 = new Hello();
+// $hello1->talk();
+
+echo SayHello::world();
---
+++ composer-quests-master/vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '610eb2b03765ed78f559c0a3c7ac8bfe5254616b',
+    'name' => 'wone/composer-5-1',
+  ),
+  'versions' => 
+  array (
+    'ehime/hello-world' => 
+    array (
+      'pretty_version' => '1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1c8cdd2c11272d8c5deec7816e51fa5374217c1',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f3b7e2c06cc05d42936a8ad508ff1db7975cdc5',
+    ),
+    'wone/composer-5-1' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '610eb2b03765ed78f559c0a3c7ac8bfe5254616b',
+    ),
+  ),
+);
